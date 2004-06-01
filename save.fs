7494a0b7114111191425175599ae50223bbf4771
jacereda <jacereda@10d27a5a-65db-0310-8c3e-83631d3227a3>
2004-06-01 11:33:49 +0000

    Save implemented with mmap-file

--- save.fs
+++ save.fs
@@ -1,42 +1,27 @@
 \ XXX Better implemented using mmap-file
 
+: d= rot = >r = r> and ;
+
 : h# ( <hexnum> -- u )
-    0 0 bl parse
-    base @ >r  hex  >number  r> base !   
-    0= invert -24 and throw 2drop
-    state @ if  postpone literal  then
+    bl parse  base @ >r  hex  s>number  r> base !   
+    state @ if dpl @ 0< if postpone literal else postpone 2literal then  then
     ; immediate
 
+: mark ( a1 -- a2 )
+   begin cell+ dup 2@ h# feedbabe.deadbeef d= until ;
 
-0 value dptr
-0 value file
-0 value resetdptr
+: save ( a u -- )
+   ['] quit cell+ dict0 cell+ cell+ !
+   w/o open-file throw >r   0 argv r/o open-file throw
+   dup mmap-file throw
+   dup dup mark over - r@ write-file throw   \ contents before dictionary
+   dict0 memtop over - r@ write-file throw + \ dictionary
+   over + >r ( handle addr r: start)
+   over file-size drop throw +
+   r@ - r> swap  r@ write-file throw drop    \ contents after dictionary
+   close-file throw  r> close-file throw ;
 
-: save-block ( bytes -- )
-   begin
-      dup
-   while
-      dptr @ h# feedbabe = dptr cell+ @ h# deadbeef = and if
-         ['] quit cell+ dict0 cell- ! 
-         ." Magic found at " dptr . cr 
-         dict0 5 cells - to dptr  0 to resetdptr
-      then
-      dptr memtop > if 256 over - pad + to dptr -1 to resetdptr then
-      dptr 1 cells file write-file throw drop
-      dptr cell+ to dptr
-      1 cells - 
-   repeat drop
-   dptr . cr resetdptr if pad to dptr then ;
+: save"
+   [char] " parse save ;
+\ save" /tmp/mierda"
 
-: save-system
-   pad to dptr -1 to resetdptr
-   s" fina" r/o open-file throw >r
-   s" /tmp/mierda" w/o open-file throw to file
-   begin  pad 256 r@ read-file throw  dup while  save-block repeat  drop
-   r> close-file throw
-   file close-file throw ;
-
-: test-mmap
-   s" /tmp/mierda" r/o open-file throw to file
-   file mmap-file . 
-   file close-file throw ;
