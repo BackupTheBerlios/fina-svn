2985699eb1f992fc6c52cade0c95f7e67d64be4c
jacereda <jacereda@10d27a5a-65db-0310-8c3e-83631d3227a3>
2004-06-01 13:28:54 +0000

    Fixed save

--- save.fs
+++ save.fs
@@ -14,14 +14,13 @@
    ['] quit cell+ dict0 cell+ cell+ !
    w/o open-file throw >r   0 argv r/o open-file throw
    dup mmap-file throw
-   dup dup mark over - r@ write-file throw   \ contents before dictionary
-   dict0 memtop over - r@ write-file throw + \ dictionary
-   over + >r ( handle addr r: start)
-   over file-size drop throw +
-   r@ - r> swap  r@ write-file throw drop    \ contents after dictionary
+   dup dup mark over - dup pad ! r@ write-file throw  \ contents before dictionary
+   dict0 memtop over - dup pad +! r@ write-file throw \ dictionary
+   dup pad @ + >r ( handle addr r: start)
+   over file-size throw drop + ( handle end r: start )
+   r@ - r> swap  r@ .s write-file throw  \ contents after dictionary
    close-file throw  r> close-file throw ;
 
 : save"
    [char] " parse save ;
-\ save" /tmp/mierda"
 
