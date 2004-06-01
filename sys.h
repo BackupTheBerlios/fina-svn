1056cbf6eb93d1d73e4574c1acca04667eee13ff
jacereda <jacereda@10d27a5a-65db-0310-8c3e-83631d3227a3>
2004-06-01 11:27:46 +0000

    More file primitives

--- sys.h
+++ sys.h
@@ -1,15 +1,26 @@
 void Sys_Init(int argc, char ** argv);
 void Sys_End();
-void * Sys_OpenFile(const char * name, unsigned mode);
-unsigned Sys_CloseFile(void * handle);
+unsigned Sys_Argc();
+char * Sys_Argv(unsigned i);
+
+// File support
+void * Sys_FileOpen(const char * name, unsigned mode);
+void Sys_FileClose(void * handle);
 unsigned Sys_FileThrow();
-unsigned Sys_ReadFile(void * handle, char * buf, unsigned len);
-unsigned Sys_WriteFile(void * handle, char * buf, unsigned len);
-void * Sys_MMapFile(void * handle);
+unsigned Sys_FileRead(void * handle, char * buf, unsigned len);
+void Sys_FileWrite(void * handle, char * buf, unsigned len);
+void * Sys_FileMMap(void * handle);
+unsigned long long Sys_FileSize(void * handle);
+unsigned long long Sys_FileTell(void * handle);
+void Sys_FileSeek(void * handle, unsigned long long pos);
+unsigned Sys_FileLine(void * handle, char * buf, unsigned len);
+
+
+// Memory
 void Sys_MemMove(char * to, const char * from, unsigned bytes);
 void Sys_MemSet(char * dst, unsigned c, unsigned bytes);
+
+// Console
 unsigned Sys_HasChar();
 unsigned Sys_GetChar();
 void Sys_PutChar(unsigned c);
-unsigned Sys_Argc();
-char * Sys_Argv(unsigned i);
