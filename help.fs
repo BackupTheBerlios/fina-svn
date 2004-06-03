8867f25ee3ccad23147e2d78eb92fb93ec40fbca
jacereda <jacereda@10d27a5a-65db-0310-8c3e-83631d3227a3>
2004-06-03 11:38:54 +0000

    Don't show TOC information

--- help.fs
+++ help.fs
@@ -17,32 +17,43 @@ variable requested 0 ,
    2dup nextword requested 2@ compare 0= >r
    bl scan bl skip nextword requested 2@ compare 0= r> or ;
 
-0 value status 
-
+0 value helpstatus 
 0 value '@see
+0 value level
+
+\ XXX
+: 0> 0 > ;
+
+: ?type level 0> and type ;
+: ?cr level 0> if cr then ;
 
 : .line? ( a u -- )
-   2dup s" ====" beginswith? if  0 to status  then
-   status 1 = if  2dup matches?  if  -1000 to status  then  then
-   status 0< if
+   2dup s" ====" beginswith? if  0 to helpstatus  then
+   helpstatus 1 = if  2dup matches?  if  -1000 to helpstatus  then  then
+   helpstatus 0< if
       2dup s" @see" beginswith? if  
-         2dup bl scan  bl skip status >r '@see execute r> to status
+         2dup bl scan  bl skip helpstatus >r '@see execute r> to helpstatus
+      else 2dup s" @also" beginswith? if
+         ." See also: " 2dup bl scan  bl skip ?type ?cr
       else
-         2dup type cr
-      then
+         2dup ?type ?cr
+      then then
    then
-   status 1+ to status
+   helpstatus 1+ to helpstatus
    2drop ;
 
 : @see ( a u -- )
-   ." <<" 2dup type ." >>" cr
+   level 1+ to level
+   s" <<" ?type 2dup ?type s" >>" ?type ?cr
    s" help/" pad place
    pad append
    s" .help" pad append 
    pad count r/o open-file throw >r
-   2 to status
+   2 to helpstatus
    r@ ['] .line? foreachline
    r> close-file throw ;  ' @see to '@see
 
-: help
+\g Display word documentation
+: help ( "word" -- )
+   -1 to level
    parse-word requested 2! s" toc" cr @see ;
