6d38d332934153ffc0c6f042c20209eaeb2e5d96
jacereda <jacereda@10d27a5a-65db-0310-8c3e-83631d3227a3>
2004-06-04 13:33:23 +0000

    Heavy rewrite

--- multi.fs
+++ multi.fs
@@ -1,8 +1,10 @@
 \ FINA multitasker
-\
-\ Originally written by Bill Muench.
-\ Adapted to hForth by Wonyong Koh
-\ Adapted to FINA by Jorge Acereda
+\ Inspired by the work of Bill Muench and Wonyong Koh
+
+\ XXX Should we have a per-task HERE?
+
+\g This is a reimplementation of 
+
 
 require assert.fs
 
@@ -10,141 +12,167 @@ require assert.fs
 
 : user ( "<spaces>name" -- ) 
    nesting?  head, xtof douser xt, drop  
-   lastuser ,  lastuser -1 cells + to lastuser
+   lastuser ,  
+   lastuser -1 cells + to lastuser
    linklast ;
+
+: ego userp @ ;
+
+\g User variable holding a pointer to current task status routine
+user status ( -- a-addr )
+
+\g User variable holding a pointer to next task's user pointer
+user follower  ( -- a-addr )
 
 \g User variable holding the stack pointer for each sleeping task
 user stacktop  ( -- a-addr )
 
-
-
-
-\ Structure of a task created by HAT
-\ userP points follower.
-\ //userP//return_stack//data_stack
-\ //user_area/user1/taskName/throwFrame/stackTop/status/follower/sp0/rp0
-
-\ //user_area/user1/taskName/throwFrame/stackTop/status/follower/sp0/rp0
-
 \   's          ( tid "uservar" -- a-addr )
 \               Index another task's USER variable
-: 's-offset ' execute userP @ - assert0( DUP -1024 1 WITHIN ) ; IMMEDIATE
-: 's 
+
+\g Obtain user offset for a given variable name
+: 's-offset  ( "uvarname" -- n )
+   ' execute ego - assert0( dup -1024 1 within ) ; immediate
+
+\g Obtain the address of a user variable in another task
+: 's  ( "uvarname" -- a-addr )
    postpone 's-offset state @ if
       postpone literal postpone +
    else + then ; immediate
 
-: findprevtask ( tid -- tid')
-    's status userp @ begin
-        2dup 's follower @ <>
-        over 's follower @ status <> and
-    while 
-        's follower @ cell+
-    repeat swap over 's follower @ = and ;
+\g Apply xt to each task in list. xt is ( tid -- tid )
+: foreachtask ( xt -- )
+   >r ego begin
+      r@ execute
+      's follower @ dup ego =
+   until rdrop drop ;
+
+: ?prevtask ( stid tid -- stid tid )
+   over over 's follower @ = if dup to found then ;
+: prevtask ( tid -- tid' )
+   0 to found
+   ['] ?prevtask foreachtask
+   drop found ;
+
+hex
+assert( : built ( tid -- ) 's follower @ 33333333 <> ; )
+assert( : active ( tid -- ) 's follower @  ; )
+decimal
 
 
 \g Stop current task and transfer control to the task of which
 \g 'status' USER variable is stored in 'follower' USER variable
 \g of current task.
 : pause  ( -- ) 
-   rp@ sp@ stacktop !  follower @ >r ; compile-only  
+   rp@ sp@ stacktop !  follower @ follower @ 's status >r ; compile-only  
 
+: restore 
+   postpone rdrop postpone userp postpone ! ; immediate compile-only
 
-\g Wake current task.
-: wake  ( -- )
-   r> userp !          \ userp points 'follower' of current task
-   stacktop @ sp!      \ set data stack
-   rp! ; compile-only  \ set return stack
+\g Status for sleeping tasks
+: sleeping ( --  r: newuserp -- )
+   restore pause ;
 
-\g Sleep current task.
+\g Status for stopped tasks
+: stopped ( --  r: newuserp -- )
+   restore pause ;
+
+\g Status for ready tasks
+: ready  ( --  r: newuserp -- )
+   restore stacktop @ sp! rp! ; compile-only 
+
+\g Stop current task
 : stop  ( -- )
-   ['] branch status ! PAUSE ;
+   ['] stopped status ! pause ; 
 
-\g Sleep another task.
+\g Set sleeping state for given task
 : sleep ( tid -- )
-    assert( dup findprevtask )
-    's status   ['] branch  SWAP ! ;
+   ['] sleeping swap 's status ! ; 
 
-\g Awake another task.
-: AWAKE  ( tid -- )
-    assert( dup findprevtask )
-    's status   ['] wake  SWAP ! ;
+\g Set ready state for given task
+: awake  ( tid -- )
+    ['] ready swap 's status ! ;
 
-\g Kill another task.
+\g Kill given task
 : kill ( tid -- )
-    >r
-    assert( r@ userp @ <> )      \ no es un suicidio
-    assert( r@ findprevtask )    \ esta en la lista
-    r@ findprevtask r@ 's follower @ swap 's follower ! 
-    assert( r@ findprevtask 0= ) \ no esta en la lista
-    rdrop ;
+   >r
+   assert( r@ built )
+   assert( r@ active )
+   assert( r@ 's follower follower <> ) \ suicide not allowed
+   r@ prevtask r@ 's follower @ swap 's follower ! 
+   r@ 's follower off
+   assert( r@ active 0= )
+   assert( r@ built )
+   rdrop ;
 
 : ,allot ( size fill -- )
    >r here over allot swap r> fill align ;
 
 \g Create a new task.
-: hat ( user_size ds_size rs_size "<spaces>name" --  rt: -- tid )
-   create 
-   2 pick aligned 2 pick aligned 2 pick aligned + + here + >r
-   r@ ,  
+: task: ( user_size ds_size rs_size "<spaces>name" --  rt: -- tid )
    [ hex ]
-   0AA ,allot here cell- r@ 's rp0 ! 
-   055 ,allot here cell- r@ 's sp0 ! 
-   r@ 's sp0 @ r@ 's rp0 @ 2>r 
-   033 ,allot
+   create here 0 , >r 
+   033 ,allot here cell- dup r> ! >r
+   0AA ,allot here cell- r@ 's rp0 !
+   055 ,allot here cell- r@ 's sp0 !
    [ decimal ]
-   2r> r@ 's rp0 ! r@ 's sp0 !
    lastname r> 's taskname ! 
    does> @ ;
 
-\g Initialize and link new task into PAUSE chain.
+\  system:follower/ego  newtask:follower/ego
+\g Initialize and link task 
 : build ( tid -- )
-    assert( dup findprevtask 0= )
-    follower @                \ current task's 'follower'
-    over 's follower !        \ store it in new task's 'follower'
-    dup 's status follower !  \ store new 'status' in current 'follower'
-    sleep ;               \ sleep new task
+   assert( dup built 0= )
+   follower @ over 's follower !
+   dup follower !  sleep ;
 
 \g Activate the task identified by tid. ACTIVATE must be used
 \g only in definition. The code following ACTIVATE must not
 \g EXIT. In other words it must be infinite loop like QUIT.
 : activate ( tid -- )
-   assert( dup findprevtask )
-   dup 's sp0 @ over 's rp0 @  
-   R> over !                   \ save entry at rp
-   over !                      \ save rp at sp
-   over 's stackTop !          \ save sp in stackTop
-   awake ; compile-only
-
-\g Display tasks list
-: .tasks ( tid -- )
-   cr follower                    \ current task's tid
-   begin
-      dup 's taskname @ .name
-      ."  at " dup hex.
-      dup 's status @  ['] wake = if
-         dup follower = if s" running " else s" awaked " then
-      else
-         s" sleeping "
-      then type cr
-      @ [ follower status - ] literal +           \ next task's follower
-      dup follower =
-    until drop ;
-
+   assert( dup built )
+   r> swap >r
+   r@ 's rp0 @ !
+   r@ 's rp0 @  r@ 's sp0 @ !
+   r@ 's sp0 @  r@ 's stacktop !
+   r> awake ; compile-only
 
 \ Initializations for system task
-' wake status !
-status follower !
-follower constant system
+' ready status !
+ego follower !
+ego constant system
 lastname taskname !
-
 
 \ Pause when no key ready
 :noname
    ekey? 0= if begin pause ekey? until then [ 'ekey @ compile, ] ;  'ekey !
 
+\g Display task information
+: .task ( tid -- tid )
+   dup 's taskname @ .name ."  at " dup hex.
+   dup 's status @ xt>name .name cr ;
+\g Display tasks list
+: .tasks
+   cr ['] .task foreachtask ;
 
-\ 88 88 88 hat mierda  mierda build
-\ : test mierda activate 0 begin 1+ dup . pause again ;
-\ test
-\ :noname 10 0 do pause loop cr ; execute
+
+64 64 64 task: mierda1
+64 64 64 task: mierda2
+64 64 64 task: mierda3
+64 64 64 task: mierda4
+
+mierda1 build 
+mierda2 build 
+mierda3 build 
+mierda4 build
+hex
+:noname mierda1 activate 4 0 do i . pause loop stop ; execute
+:noname mierda2 activate 0 begin 1+ dup . pause again ; execute
+:noname mierda3 activate 0 begin 1+ dup . pause again ; execute
+:noname mierda4 activate 0 begin 1+ dup . pause again ; execute
+.tasks 
+:noname pause ; execute
+mierda3 sleep
+mierda4 kill
+.tasks
+:noname 5 0 do .tasks pause loop cr ; execute
