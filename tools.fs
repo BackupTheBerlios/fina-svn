f1d1cba2c013a2210acbe5c4623ac70aff38ceaa
jacereda <jacereda@10d27a5a-65db-0310-8c3e-83631d3227a3>
2004-06-08 14:23:50 +0000

    The beginnings of SEE

--- tools.fs
+++ tools.fs
@@ -1,3 +1,21 @@
+\g @see anstools
+: ?  ( a-addr -- )
+   @ . ;
+
+\g Type name of word at NFA
+: .name ( nfa -- )
+   namecount type space ;
+
+: (.name) ( nfa -- nfa )
+   dup .name 
+   1 here +! ;
+
+\g @see anstools
+: words ( -- )
+   here off
+   ['] (.name) forwords 
+   cr here @ . ." words" cr ;
+
 : dumprow ( addr u -- addr' )
   swap cr dup 0 <#  # # # # # # # #  #> type space
   16 0 do
@@ -18,3 +36,44 @@
    0 ?do 16 dumprow loop
    R> dumprow drop r> base ! ;
 
+: (next?)
+   2dup cell- @ = over and to found ;
+
+: nextnfa ( nfa1 -- nfa2 )
+   ['] (next?) forwords drop found ;
+
+: /xt ( xt -- a-addr )
+   xt>name nextnfa cell- ;
+
+: type? ( addr <inline-doer> -- flag
+   ?dodefine nip @r+ = ;
+
+: doersee ( xt -- )
+   dup type? dolist if ." : " then
+   dup type? dovar if ." variable " then
+   dup type? doconst if ." constant " then
+   dup type? douser if ." user " then
+   dup type? dovalue if ." value " then
+   drop ;
+
+: xt? ( val -- flag )
+   dup aligned over <> if drop 0 exit then
+   dup dict? 0= if primxt? else ?dodefine nip 0<> then ;
+
+: safext>name ( xt -- name|0 )
+   dup xt? if xt>name else drop c" (null)" then ;
+
+: cellsee ( a-addr1 -- a-addr2 )
+   dup @ safext>name .name 
+   cell+ ;
+
+: xtsee ( xt -- )
+   dup doersee 
+   dup xt>name .name
+   dup /xt swap ?dodefine drop 
+   begin  2dup >  while  cellsee  repeat ;
+
+\g @see anstools
+: see
+   ' /fcompo fimmed rot xtsee 
+   0 > if ." immediate "  then    0= if ." compile-only" then cr ;
