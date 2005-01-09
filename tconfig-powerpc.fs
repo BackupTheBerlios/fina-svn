fdb44fd6664499ad6c0335d1cd2ce0915b34712e
jacereda <jacereda@10d27a5a-65db-0310-8c3e-83631d3227a3>
2005-01-09 19:30:14 +0000

    Linux/ppc support

--- tconfig-powerpc.fs
+++ tconfig-powerpc.fs
@@ -16,14 +16,23 @@
 : .align ."  .align 2" cr ;
 : .init 
    ."  .globl _Forth_Entry " cr 
+   ."  .globl Forth_Entry " cr 
    ."  .globl _Forth_UserP" cr 
+   ."  .globl Forth_UserP" cr 
    ."  .globl _Forth_Here" cr 
+   ."  .globl Forth_Here" cr 
    ."  .data" cr
    .align
    ."  .long 0xfeedbabe, 0xdeadbeef" cr
-   ." _Forth_Entry: .long XT_COLD" cr
-   ." _Forth_UserP: .long XT_USERP" cr
-   ." _Forth_Here: .long XT_HERE" cr
+   ." _Forth_Entry:" cr
+   ." Forth_Entry:" cr
+   ."  .long XT_COLD" cr
+   ." _Forth_UserP:" cr
+   ." Forth_UserP:" cr 
+   ."  .long XT_USERP" cr
+   ." _Forth_Here:" cr
+   ." Forth_Here:" cr
+   ."  .long XT_HERE" cr
 ;
 
 : .end ;
