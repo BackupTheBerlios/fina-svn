813fc8adce0dfa438e01c16691a270691b4bdc31
jacereda <jacereda@10d27a5a-65db-0310-8c3e-83631d3227a3>
2005-01-12 01:54:38 +0000

    handle C-h

--- lineedit.fs
+++ lineedit.fs
@@ -3,16 +3,17 @@ variable echo  echo off
    over span @ + ;
 : bell 7 emit ;
 : bs 8 emit ;
-: del s"  " type ;
 : +span ( u offset -- u flag ) 
    span @ + >r 
    dup r@ -1 within dup if r@ span ! else bell then rdrop ;
+: del -1 +span if bs space bs then ;
 :noname ( c-addr u char -- c-addr u char )
    ekey>char case
       1 of  span @ 0 ?do bs loop span @ negate +span drop  1 endof
       2 of  -1 +span if bs then  2 endof
       6 of  1 +span if cursor 1- c@ emit then  6 endof
-      127 of  -1 +span if del then  127 endof
+      8 of  del  127  endof
+      127 of  del  127 endof
       printable echo @ if 
          dup if dup emit else bell then 
       then dup 
