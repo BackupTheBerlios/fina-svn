31281ed03ccf748515d1e9bae1874c87349b11b0
jacereda <jacereda@10d27a5a-65db-0310-8c3e-83631d3227a3>
2005-01-12 01:55:31 +0000

    handle C-h

--- lineedit.fs
+++ lineedit.fs
@@ -12,7 +12,7 @@ variable echo  echo off
       1 of  span @ 0 ?do bs loop span @ negate +span drop  1 endof
       2 of  -1 +span if bs then  2 endof
       6 of  1 +span if cursor 1- c@ emit then  6 endof
-      8 of  del  127  endof
+      8 of  del  8  endof
       127 of  del  127 endof
       printable echo @ if 
          dup if dup emit else bell then 
